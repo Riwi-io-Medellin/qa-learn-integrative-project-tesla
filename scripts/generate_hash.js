455b94276838729d294cc97ed290e3ad7f517fd9
FernandaHiguita <fernandahiguita234@gmail.com>
2026-03-11 15:06:38 -0500

    Full auth functionality

---
+++ scripts/generate_hash.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcryptjs";
+
+const password = 'Admin123*'
+const hash = await bcrypt.hash(password, 10)
+
+console.log('hash generado: ')
+console.log(hash)
+
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { connectPostgres } from "./config/db.js";
+import express from 'express'; 
+import { authRoutes } from "./modules/auth/auth.routes.js";
+
+await connectPostgres(); 
+
+const app = express(); 
+
+app.use(express.json()); 
+
+app.use('/auth', authRoutes)
+
+app.get("/health", (req,res) => {
+    res.json({ status: "ok"}); 
+}); 
+
+export default app; 
