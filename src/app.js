736ad26da801901c551f34757e2461a23a7c98ad
Mejia Sebastian <mejiasebastian333@gmail.com>
2026-03-12 17:48:11 -0500

    Resuelve conflicto en app.js y conserva rutas de auth y projects

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import { connectPostgres } from "./config/db.js";
 import express from 'express'; 
 import { authRoutes } from "./modules/auth/auth.routes.js";
-<<<<<<< HEAD
-import { projectsRoutes }     from "./modules/projects/projects.routes.js";
-=======
->>>>>>> 06427c379f81f0ba1b79067b5bd328bf8a88d4fa
+import { projectsRoutes } from "./modules/projects/projects.routes.js";
 
 await connectPostgres(); 
 
@@ -12,13 +9,8 @@ const app = express();
 
 app.use(express.json()); 
 
-<<<<<<< HEAD
 app.use('/auth',                                      authRoutes);
 app.use('/api/projects',                              projectsRoutes);
-
-=======
-app.use('/auth', authRoutes)
->>>>>>> 06427c379f81f0ba1b79067b5bd328bf8a88d4fa
 
 app.get("/health", (req,res) => {
     res.json({ status: "ok"}); 
